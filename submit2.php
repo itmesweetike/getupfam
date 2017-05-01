41eae61d1097d3430b0522b62fe3c521e5839e10
Cameron Mitchell <mitchellcameron6@gmail.com>
2017-05-01 19:02:46 -0400

    Update submit2.php
    Added in the quotes for the file name, and removed the var_dump at the end

--- submit2.php
+++ submit2.php
@@ -1,9 +1,6 @@
 <?php
- 
-    $data = $_POST['firstname'] . '-' . $_POST['Apikey'] . "\n";
-    $openthefile = fopen(thedata.txt, "a");
-    fwrite($openthefile, $data);
-    fclose($openthefile);
-    var_dump($data);
-
+    	$data = $_POST['firstname'] . '-' . $_POST['Apikey'] . '-' .$_POST['Whenareyouoff'] . '-' . $_POST['Schedule'] . "\n";
+   		$openthefile = fopen(".thedata.txt", "a");
+   		fwrite($openthefile, $data);
+    	fclose($openthefile);
 ?>
